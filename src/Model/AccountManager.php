65cf2835b4c931b3113207973dcc9782c1e73f86
Marie <Marue59@hotmail.fr>
2021-08-26 14:56:55 +0200

    account page

---
+++ src/Model/AccountManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Portfolio\Model;
+use \PDO;
+use Portfolio\Entity\User;
+use Portfolio\Model\Database;
+
+class AccountManager extends Database 
+{
+    private $table = "users";
+    
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+
+}   
+
